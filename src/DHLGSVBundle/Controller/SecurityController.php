2655b9695b44373ae6e5196ec58d897b5a927844
Diamond1826 <diamond1826@hotmail.com>
2017-08-16 18:09:51 +0200

    add login, logout and CSRF-Protection

---
+++ src/DHLGSVBundle/Controller/SecurityController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DHLGSVBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityController extends Controller
+{
+	/**
+     * @Route("/login", name="login")
+     * @Template()
+     */
+    public function loginAction(Request $request)
+    {
+    	$authenticationUtils = $this->get('security.authentication_utils');
+
+	    // get the login error if there is one
+	    $error = $authenticationUtils->getLastAuthenticationError();
+
+	    // last username entered by the user
+	    $lastUsername = $authenticationUtils->getLastUsername();
+
+	    return $this->render('security/login.html.twig', array(
+	        'last_username' => $lastUsername,
+	        'error'         => $error,
+	    ));
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     * @Template()
+     */
+    public function logoutAction()
+    {
+    	return $this->redirectToRoute('home');
+    }
+}
---
+++ src/DHLGSVBundle/Tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DHLGSVBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+}
